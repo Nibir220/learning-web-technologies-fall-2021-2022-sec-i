9a7803444c964531b6bde9f49fc69a341b210f9c
Nibir <81643180+Nibir220@users.noreply.github.com>
2021-11-30 16:59:03 +0600

    Add files via upload
    jquery

---
+++ FINAL_LAB_TASK_JQUERY/script.js
@@ -0,0 +1,24 @@
+
+$('#submit').click(function(){
+	alert($('#name').val());
+});
+
+$('#submit').click(function(){
+	alert($('#email').val());
+});
+
+$('#submit').click(function(){
+	alert($('#gender').val());
+});
+
+$('#submit').click(function(){
+	alert($('#dob').val());
+});
+
+$('#submit').click(function(){
+	alert($('#degree').val());
+});
+
+$('#submit').click(function(){
+	alert($('#picture').val());
+});
